338dd2756bc391b3cdd0745430bd70a7dc8a2f78
Aby Sucahyo <aby@gamatechno.com>
2023-02-06 08:07:00 +0700

    initial commit

---
+++ middleware/auth_middleware.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"belajar-golang-restful-api/helper"
+	"belajar-golang-restful-api/model/web"
+	"net/http"
+)
+
+type AuthMiddleware struct {
+	Handler http.Handler
+}
+
+func NewAuthMiddleware(handler http.Handler) *AuthMiddleware {
+	return &AuthMiddleware{
+		Handler: handler,
+	}
+}
+
+func (middleware *AuthMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	if request.Header.Get("X-API-Key") == "SECRET" {
+		//ok
+		middleware.Handler.ServeHTTP(writer, request)
+	} else {
+		//error
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusUnauthorized)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusUnauthorized,
+			Status: "Unauthorized",
+		}
+
+		helper.WriteToResponseBody(writer, webResponse)
+	}
+}
